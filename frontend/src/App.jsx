0acd4e0463b73c5742db8851bb095b84c980acdc
Andreea-Viktorya Dobre <andreea.viktorya@gmail.com>
2023-11-19 02:04:09 +0200

    Footer + Testoasa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import "./App.css";
 
 import Navbar from "./components/Navbar/Navbar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
+
+import Testoasa from "./assets/testoasa.png";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="page">
+        <div className="header">
+          <p>
+            Opiniile sunt mai importante ca niciodată. Platformele de sondaje
+            permit organizatorilor să culeagă feedback direct de la audiența lor
+            și să înțeleagă mai bine nevoile și dorințele acesteia.
+          </p>
+          <div>
+            <img className="testoasa" src={Testoasa} />
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
