6196fcacb629e125a668d97d1f5305ff27f2da38
Andreea-Viktorya Dobre <andreea.viktorya@gmail.com>
2023-12-06 23:59:22 +0200

    done

--- frontend/src/components/Login/AuthContext.jsx
+++ frontend/src/components/Login/AuthContext.jsx
@@ -1,81 +1,59 @@
-// import { createContext, useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-
-// export const AuthProvider = ({ children }) => {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     // verifica dacă există un token în localStorage
-//     const token = localStorage.getItem("authToken");
-//     // daca exista, isLoggedIn = true
-//     if (token) {
-//       setIsLoggedIn(true);
-//     }
-//   }, []);
-
-//   const onLogin = (data) => {
-//     setIsLoggedIn(true);
-//     localStorage.setItem("authToken", data.token);
-//     navigate("/", { replace: true });
-//   };
-
-//   const onLogout = () => {
-//     setIsLoggedIn(false);
-//     localStorage.removeItem("authToken");
-//     navigate("/", { replace: true });
-//   };
-
-//   return (
-//     <AuthContext.Provider value={{ isLoggedIn, onLogin, onLogout }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-// export default AuthContext;
-
-// AuthProvider.jsx
-
-import { createContext, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { createContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const AuthContext = createContext({
   isLoggedIn: false,
+  user: null,
   onLogin: () => {},
-  onLogout: () => {},
+  onLogout: () => {}
 });
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null); 
   const navigate = useNavigate();
 
   useEffect(() => {
-    const token = localStorage.getItem("authToken");
+    const token = localStorage.getItem('authToken');
     if (token) {
       setIsLoggedIn(true);
+      fetchUserDetails(token);
     }
   }, []);
 
+  const fetchUserDetails = async (token) => {
+    try {
+      const response = await fetch('/api/user', {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setUser(data); 
+      }
+    } catch (error) {
+      console.error('Eroare la obținerea detaliilor utilizatorului:', error);
+    }
+  };
+
   const onLogin = (data) => {
     setIsLoggedIn(true);
-    localStorage.setItem("authToken", data.token);
-    navigate("/", { replace: true });
+    localStorage.setItem('authToken', data.token);
+    fetchUserDetails(data.token); 
+    navigate('/', { replace: true });
   };
 
   const onLogout = () => {
     setIsLoggedIn(false);
-    localStorage.removeItem("authToken");
-    navigate("/", { replace: true });
-  };
-
-  const contextValue = {
-    isLoggedIn,
-    onLogin,
-    onLogout,
+    setUser(null); 
+    localStorage.removeItem('authToken');
+    navigate('/', { replace: true });
   };
 
   return (
-    <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ isLoggedIn, user, onLogin, onLogout }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
-export default AuthProvider;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,20 +1,25 @@
-import React, { useContext, useState } from "react";
+import { useState, useContext } from "react";
 import Logo from "../../assets/logo.png";
-import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../Login/AuthContext.jsx";
+import { Link, useNavigate } from "react-router-dom";
 
 import "./Navbar.css";
 import { Login } from "../Login/Login";
 import { Register } from "../Register/Register";
 import { CreatePoll } from "../CreatePoll/createPoll";
-import { AuthContext } from "../Login/AuthContext";
 
 function Navbar() {
-  const { isLoggedIn } = useContext(AuthContext);
-  const navigate = useNavigate();
+  const { isLoggedIn, onLogin, onLogout } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    onLogout();
+    navigate("/");
+  };
+
   const [showMenu, setShowMenu] = useState(false);
 
   function toggleMobileMenu(menu) {
-    console.log(typeof this);
+    menu.classList.toggle("open");
   }
 
   const closeMenu = () => {
@@ -24,33 +29,53 @@ function Navbar() {
   return (
     <div className="nav-navbar">
       <img className="logo" src={Logo} alt="Logo" />
-
-      <div className="text">
-        <ul>
-          {!isLoggedIn ? (
-            <>
-              <Login />
+      <div className="nav-links">
+        {!isLoggedIn ? (
+          <>
+            <div className="nav-item">
+              <Login onLogin={onLogin} /> {/* Pass onLogin here */}
+            </div>
+            <div className="nav-item">
               <Register />
-            </>
-          ) : (
-            <>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="nav-item">
               <CreatePoll />
+            </div>
+            <div className="nav-item">
               <button onClick={handleLogout}>Logout</button>
+            </div>
+          </>
+        )}
+      </div>
+      <div
+        id="hamburger-icon"
+        onClick={() =>
+          toggleMobileMenu(document.getElementById("hamburger-icon"))
+        }
+      >
+        <ul className="mobile-menu">
+          {!isLoggedIn && (
+            <>
+              <li id="login">
+                <Login onLogin={onLogin} /> {/* Pass onLogin here */}
+              </li>
+              <li id="register">
+                <Register />
+              </li>
             </>
           )}
-        </ul>
-      </div>
-      <div id="hamburger-icon" onClick={toggleMobileMenu(this)}>
-        <ul className="mobile-menu">
-          <li id="login">
-            <Login />
-          </li>
-          <li id="register">
-            <Register />
-          </li>
+          {isLoggedIn && (
+            <li id="logout">
+              <button onClick={handleLogout}>Logout</button>
+            </li>
+          )}
         </ul>
       </div>
     </div>
   );
 }
+
 export default Navbar;
